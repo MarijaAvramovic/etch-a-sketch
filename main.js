efd90496f3e3098cff84a75ab5b0983832bfac29
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-08-13 20:33:37 -0700

    set up hover efect to square divs

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 console.log("hi");
 
 const container = document.querySelector('#container');
- 
+
 
 for(let i = 0; i <= 34; i++) {
     const squareDiv = document.createElement('div');
     squareDiv.classList.add('square-fixed');
     container.appendChild(squareDiv);
 }
+const hoveringDivs = document.querySelectorAll('.square-fixed');
+
+
+hoveringDivs.forEach(div => {
+    div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = 'lightblue';
+    });
+    div.addEventListener('mouseleave', () => {
+        div.style.backgroundColor = '';
+    });
+});
