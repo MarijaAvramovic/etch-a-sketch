7f1867f01d4cc3e403553ec4debd73e4c47eced9
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-08-13 20:18:35 -0700

    center content in div

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log("hi");
 const container = document.querySelector('#container');
  
 
-for(let i = 0; i < 16; i++) {
+for(let i = 0; i <= 34; i++) {
     const squareDiv = document.createElement('div');
     squareDiv.classList.add('square-fixed');
     container.appendChild(squareDiv);
