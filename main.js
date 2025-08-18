b6bafcc3a3d6944f7f5d3245715338df575e7353
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-08-17 20:38:15 -0700

    create 16x16 even divs fix old code

--- main.js
+++ main.js
@@ -4,25 +4,30 @@ const container = document.querySelector('#container');
 const btn = document.querySelector('button');
 
 
-for(let i = 0; i <= 34; i++) {
+for(let i = 0; i <= 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for(let j = 0; j <= 16; j++) {
     const squareDiv = document.createElement('div');
-    squareDiv.classList.add('square-fixed');
-    container.appendChild(squareDiv);
+    squareDiv.classList.add('square-div');
+    row.appendChild(squareDiv);
+    }
+    container.appendChild(row);
 }
-const hoveringDivs = document.querySelectorAll('.square-fixed');
+
+const hoveringDivs = document.querySelectorAll('.square-div');
 
 
 hoveringDivs.forEach(div => {
     div.addEventListener('mouseenter', () => {
         div.style.backgroundColor = 'lightblue';
     });
-    // div.addEventListener('mouseleave', () => {
-    //     div.style.backgroundColor = '';
-    // });
+
 });
 
 btn.addEventListener('click', () => {
-    let userInput = prompt("Please enter the number of dquares per side:");
+    let userInput = prompt("Please enter the number of squares per side:");
     let integerValue = parseInt(userInput, 10);
     container.remove();
     
